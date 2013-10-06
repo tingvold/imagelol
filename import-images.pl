b933fee1ff229eab12f0249e5fe97e04bcb21b1b
Joachim Tingvold <joachim@tingvold.com>
2013-10-06 09:26:45 +0200

    Fixed threading.

--- import-images.pl
+++ import-images.pl
@@ -6,6 +6,9 @@ use Getopt::Long;
 use File::Find;
 use Image::ExifTool;
 use File::stat;
+use threads;
+use threads::shared;
+use Thread::Queue;
 
 # Load imagelol
 my $imagelol_dir;
@@ -14,6 +17,14 @@ use lib $imagelol_dir;
 use imagelol;
 my $imagelol = imagelol->new();
 my %config = $imagelol->get_config();
+
+# Variables
+my $max_threads = 5;			# Max threads to use
+my $imageq = Thread::Queue->new(); 	# Queue to put image files in
+#my $switches : shared = 0;		# Number of successful switches
+#my $failed_switches : shared = 0;	# Number of failed switches
+#my $total_time : shared = 0;		# Total time spent for all switches
+#my %telnet_sessions : shared;		# Number of sessions currently in use
 
 # Log
 sub log_it{
@@ -44,132 +55,162 @@ if ($category){
 	die error_log("Invalid category. Only numbers and letters allowed.") unless ($category =~ m/^[a-zA-Z0-9]$/);
 }
 
-$category = $config{path}->{default_category} unless $category;
+$category = $config{div}->{default_category} unless $category;
 $src_dir = $config{path}->{import_folder} unless $src_dir;
 $dst_dir = $config{path}->{original_folder} . "/" . $category;
 
 die error_log("Source directory doesn't exist. Exiting.") unless (-d $src_dir);
 
-# Import images
-sub import_images{
+# Find images
+sub find_images{
 	# Find and import all images
-	find(\&copy_images, $src_dir);
+	find(\&image_queue, $src_dir);
+}
+
+# Add images to queue
+sub image_queue{
+	my %image = (
+		full_path => "$File::Find::name",
+		image_file => "$_",
+	);
+	
+	# Add image to queue
+	$imageq->enqueue(\%image);
 }
 
 # Copy images
-sub copy_images{
-	my $image_full_path = "$File::Find::name";
-	my $image_file = "$_";
-	
-	if ($image_file =~ m/^.+\.$config{div}->{image_filenames}$/i){
-		# We have a image (or, at least a filename that matches the image_filenames variable)
+sub process_images{
+	while (my $image = $imageq->dequeue()){
+		last if ($image eq 'DONE');	# all done
 		
-		# We need to figure out the date the picture was taken
-		# First we try to use EXIF, and if that fails, we use the 'file created'
-		my $exif_tags = Image::ExifTool::ImageInfo(	$image_full_path, { PrintConv => 0 },
-								'DateTimeOriginal','PreviewImage','Orientation');
-		die error_log("EXIF failed: $exif_tags->{Error}") if $exif_tags->{'Error'};
+		if ($image->{image_file} =~ m/^.+\.($config{div}->{image_filenames})$/i){
+			# We have a image (or, at least a filename that matches the image_filenames variable)
 
-		my $date;
-		if (defined($exif_tags->{'DateTimeOriginal'})){
-			# We have a value
-			# 'DateTimeOriginal' => '2012:12:31 17:50:01',
-			
-			if ($exif_tags->{'DateTimeOriginal'} =~ m/^[0-9]{4}\:[0-9]{2}\:[0-9]{2}\s+/){
-				$date = (split(' ', $exif_tags->{'DateTimeOriginal'}))[0];
+			# Is this a RAW image? (affects what we do with preview, etc)
+			my $is_raw = 0;
+			$is_raw = 1 if ($image->{image_file} =~ m/^.+\.($config{div}->{raw_image})$/i);
+
+			# We need to figure out the date the picture was taken
+			# First we try to use EXIF, and if that fails, we use the 'file created'
+			my $exif_tags = Image::ExifTool::ImageInfo(	$image->{full_path}, { PrintConv => 0 },
+									'DateTimeOriginal','PreviewImage','Orientation');
+									### TODO, if the above fails, we need to fetch specific
+									### tags depending on wether or not it's a raw file
+			die error_log("EXIF failed: $exif_tags->{Error}") if $exif_tags->{'Error'};
+
+			my $date;
+			if (defined($exif_tags->{'DateTimeOriginal'})){
+				# We have a value
+				# 'DateTimeOriginal' => '2012:12:31 17:50:01',
+
+				if ($exif_tags->{'DateTimeOriginal'} =~ m/^[0-9]{4}\:[0-9]{2}\:[0-9]{2}\s+/){
+					$date = (split(' ', $exif_tags->{'DateTimeOriginal'}))[0];
+				}
 			}
-		}
-		
-		unless ($date){
-			# No (valid) date was found with EXIF
-			# Using 'file created'
-			
-			# Last access:		stat($_)->atime
-			# Last modify:		stat($_)->mtime
-			# File creation:	stat($_)->ctime
-			
-			# Use YYYY:MM:DD, so that it's the same output as EXIF
-			$date = POSIX::strftime("%Y:%m:%d", localtime(stat($image_full_path)->atime()));
-		}		
-		
-		# At this point it should be safe to assume that $date has a value
-		# And that it looks like YYYY:MM:DD
-		my ($year, $month, $day) = split(':', $date);
-		
-		# We now know the dates when the file was created
-		# Check if directory exists, if not, create it
-		my $image_dst_dir = $dst_dir . "/" . $year . "/" . $month . "/" . $day;
-		unless (-d $image_dst_dir){
-			# create directory
-			log_it("Creating directory '$image_dst_dir'.");
-			$imagelol->system_mkdir($image_dst_dir);
-		}
-				
-		# Check if destination image exists
-		my $image_dst_file = $image_dst_dir . "/" . $image_file;
-		die error_log("Destination file ($image_dst_file) exists. Aborting.") if (-e $image_dst_file);
-		
-		# Copy image
-		log_it("Copying image '$image_full_path' to '$image_dst_file'.");
-		$imagelol->copy_stuff($image_full_path, "$image_dst_dir/");
-		
-		# Extract preview
-		# Save full version + resized version
-		log_it("Extracting preview from RAW file.");
 
-		# Exit if error
-		die error_log("No preview found.") unless defined($exif_tags->{'PreviewImage'});
+			unless ($date){
+				# No (valid) date was found with EXIF
+				# Using 'file created'
 
-		# Fetch JPG
-		my $jpg_from_raw = $exif_tags->{'PreviewImage'};
-		$jpg_from_raw = $$jpg_from_raw if ref($jpg_from_raw);
+				# Last access:		stat($_)->atime
+				# Last modify:		stat($_)->mtime
+				# File creation:	stat($_)->ctime
 
-		# Create dir
-		my $preview_dst_dir = $config{path}->{preview_folder} . "/" . $category . "/" . $year . "/" . $month . "/" . $day;
-		unless (-d $preview_dst_dir){
-			# create directory
-			log_it("Creating directory '$preview_dst_dir'.");
-			$imagelol->system_mkdir($preview_dst_dir);
-		}
+				# Use YYYY:MM:DD, so that it's the same output as EXIF
+				$date = POSIX::strftime("%Y:%m:%d", localtime(stat($image->{full_path})->ctime()));
+			}		
 
-		# Make filename of previews
-		(my $jpg_filename = $image_file) =~ s/\.[^.]+$//;
-		my $jpg_filename_full = $jpg_filename . "-full" . ".jpg";
-		my $jpg_filename_small = $jpg_filename . "-small" . ".jpg";
-		
-		my $jpg_dst_full = $preview_dst_dir . "/" . $jpg_filename_full;
-		my $jpg_dst_small = $preview_dst_dir . "/" . $jpg_filename_small;
-		
-		# Copy full preview
-		my $JPG_FILE;
-		open(JPG_FILE,">$jpg_dst_full") or die error_log("Error creating '$jpg_dst_full'.");
-		binmode(JPG_FILE);
-		my $err;
-		print JPG_FILE $jpg_from_raw or $err = 1;
-		close(JPG_FILE) or $err = 1;
-		if ($err) {
-			unlink $jpg_dst_full; # remove the bad file
-			die error_log("Could not copy preview image '$jpg_dst_full'. Aborting.");
+			# At this point it should be safe to assume that $date
+			# has a value, and that it looks like YYYY:MM:DD
+			my ($year, $month, $day) = split(':', $date);
+
+			# We now know the dates when the file was created
+			# Check if directory exists, if not, create it
+			my $image_dst_dir = $dst_dir . "/" . $year . "/" . $month . "/" . $day;
+			unless (-d $image_dst_dir){
+				# create directory
+				log_it("Creating directory '$image_dst_dir'.");
+				$imagelol->system_mkdir($image_dst_dir);
+			}
+
+			# Check if destination image exists
+			my $image_dst_file = $image_dst_dir . "/" . $image->{image_file};
+			die error_log("Destination file ($image_dst_file) exists. Aborting.") if (-e $image_dst_file);
+
+			# Copy image
+			log_it("Copying image '$image_full_path' to '$image_dst_file'.");
+			$imagelol->copy_stuff($image->{full_path}, "$image_dst_dir/");
+
+			# Extract preview
+			# Save full version + resized version
+			log_it("Extracting preview from RAW file.") if $is_raw;
+
+			# Exit if error
+			die error_log("No preview found.") unless defined($exif_tags->{'PreviewImage'});
+
+			# Fetch JPG
+			if ($is_raw){
+				my $jpg_from_raw = $exif_tags->{'PreviewImage'};
+				$jpg_from_raw = $$jpg_from_raw if ref($jpg_from_raw);
+			}
+
+			# Create dir
+			my $preview_dst_dir = $config{path}->{preview_folder} . "/" . $category . "/" . $year . "/" . $month . "/" . $day;
+
+			unless (-d $preview_dst_dir){
+				# create directory
+				log_it("Creating directory '$preview_dst_dir'.");
+				$imagelol->system_mkdir($preview_dst_dir);
+			}
+
+			# Make filename of previews
+			(my $jpg_filename = $image->{image_file}) =~ s/\.[^.]+$//;
+			my $jpg_filename_full = $jpg_filename . "-full" . ".jpg";
+			my $jpg_filename_small = $jpg_filename . "-small" . ".jpg";
+							## TODO: Use 'full' for full, and then size for the resized one?
+							## This way we can more easily add multiple image-sizes at a later point
+
+			my $jpg_dst_full = $preview_dst_dir . "/" . $jpg_filename_full;
+			my $jpg_dst_small = $preview_dst_dir . "/" . $jpg_filename_small;
+
+			# Copy full preview
+			if ($is_raw){
+				# Extract preview from RAW file
+				my $JPG_FILE;
+				open(JPG_FILE,">$jpg_dst_full") or die error_log("Error creating '$jpg_dst_full'.");
+				binmode(JPG_FILE);
+				my $err;
+				print JPG_FILE $jpg_from_raw or $err = 1;
+				close(JPG_FILE) or $err = 1;
+				if ($err) {
+					unlink $jpg_dst_full; # remove the bad file
+					die error_log("Could not copy preview image '$jpg_dst_full'. Aborting.");
+				}
+			} else {
+				# Copy normally, as source is non-RAW
+				$imagelol->copy_stuff($image_full_path, $jpg_dst_full);
+			}
+
+			# Copy EXIF-data from source, into preview
+			$imagelol->copy_exif($image->{full_path}, $jpg_dst_full);
+
+			# Rotate full preview (if needed)
+			# http://sylvana.net/jpegcrop/exif_orientation.html
+			# http://www.impulseadventure.com/photo/exif-orientation.html
+			# We do rotation unless 'orientation == 1'	
+			my $rotate = 1;
+			if (defined($exif_tags->{'Orientation'})){
+				$rotate = 0 if ($exif_tags->{'Orientation'} == 1);
+			}
+			$imagelol->rotate_image($jpg_dst_full, $jpg_dst_full) if $rotate;
+
+			# Make resized preview
+			$imagelol->resize_image($config{image}->{medium_width}, $config{image}->{medium_height}, $jpg_dst_full, $jpg_dst_small);
 		}
-		
-		# Copy EXIF-data from RAW, into preview
-		$imagelol->copy_exif($image_full_path, $jpg_dst_full);
-		
-		# Rotate full preview (if needed)
-		# http://sylvana.net/jpegcrop/exif_orientation.html
-		# http://www.impulseadventure.com/photo/exif-orientation.html
-		# We do rotation unless 'rotation == 1'	
-		my $rotate = 0;
-		if (defined($exif_tags->{'Orientation'})){
-			$rotate = 1 unless ($exif_tags->{'Orientation'} == 1);
-		} else {
-			$rotate = 1;
-		}
-		$imagelol->rotate_image($jpg_dst_full, $jpg_dst_full) if $rotate;
-		
-		# Make resized preview
-		$imagelol->resize_image($config{image}->{medium_width}, $config{image}->{medium_height}, $jpg_dst_full, $jpg_dst_small);
 	}
+	
+  	# detach thread -- we're done
+	threads->detach;
 }
 
 # We only want 1 instance of this script running
@@ -181,8 +222,17 @@ unless (flock(DATA, LOCK_EX|LOCK_NB)) {
 # Let's start...
 my $time_start = time();
 
-# Import images...
-import_images();
+# Find all images, add to queue
+find_images();
+
+# Let the threads know when they're done
+$imageq->enqueue("DONE") for (1..$max_threads);
+
+# Start processing the queue
+threads->create("process_images") for (1..$max_threads);
+
+# Wait till all threads is done
+sleep 5 while (threads->list(threads::running));
 
 # How long did we run
 my $runtime = time() - $time_start;
