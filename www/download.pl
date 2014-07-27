0bb34970fda51c5e9ac83223b34c3299443f8dd2
Joachim Tingvold <joachim@tingvold.com>
2014-07-28 01:26:51 +0200

    Working zip-album-on-the-fly.

--- www/download.pl
+++ www/download.pl
@@ -6,6 +6,8 @@ use Getopt::Long;
 use File::Find;
 use File::Basename;
 use Encode;
+use Archive::Zip;
+use CGI;
 
 # Load imagelol
 my $imagelol_dir;
@@ -13,6 +15,7 @@ BEGIN { $imagelol_dir = "/srv/bilder/imagelol"; }
 use lib $imagelol_dir;
 use imagelol;
 my $imagelol = imagelol->new();
+$imagelol->enable_silent_logging(); # silent
 my %config = $imagelol->get_config();
 
 # Log
@@ -33,34 +36,73 @@ sub error_log{
 
 # ZIP an entire album on-the-fly
 sub zip_album{
-	my $path = "/srv/bilder/arkiv/preview/6D/2014/06/19/IMG_8873.jpg";
-	my $filename = "test.zip";
+	my $album = shift;
+	(my $prettyname = lc($album->{name})) =~ s/[^a-zA-Z]+//g; # strip all but characters
+	$prettyname = substr($prettyname, 0, 10); # limit filename to 10 chars
+	my $filename = "album-" . $prettyname . ".zip";
+	
 	my $zip = Archive::Zip->new();
-	$zip->addTree( $path, '' );
+
+	# find all images in album
+	my %empty;
+	my $images = $imagelol->merge_image_ranges(\%empty, $album, $album->{name}, "foo", "bar");
+
+	foreach my $image (keys %$images){
+		my $filename = $images->{$image}{path_preview};
+		$filename =~ s/.+\/(.+)$/$1/; # get filename to use in zip
+		
+		$zip->addFile($images->{$image}{path_preview}, $filename);
+	}
 
 	# set binmode
 	binmode STDOUT;
 	
-	# flush headers
+	# header
+	print "Content-type: application/octet-stream\n";
+	print "Content-Disposition: attachment; filename=\"$filename\"\n\n"; # need extra line after header
+	
+	# flush stdout after the header
 	$|=1;
 	
-	# header
-	#Content-type: application/zip\n"
-	#"Content-Disposition: attachment; filename=\"$filename\"\n\n";
-		
 	# sendt to browser
 	$zip->writeToFileHandle( \*STDOUT, 0 );
 }
 
+# print 404
+sub not_found{
+	my $header = CGI::header(
+		-type => 'text/html',
+		-status => '404 Not Found',
+		-charset => 'utf-8',
+	);
+	
+	print $header;
+	print "Album not found.\n";
+}
 
 # Let's start...
-#my $time_start = time();
-#$imagelol->connect();
+$imagelol->connect();
 
+# fetch album name
+my $cgi = CGI->new();
+my $album_name = $cgi->param('n');
 
-zip_album();
+# Check if album exists
+my $album = $imagelol->get_album($album_name);
+if($album){
+	if($album->{enabled}){
+		# valid album + enabled
+		zip_album($album);
+	} else {
+		# album not enabled
+		# return 404 not found
+		not_found();
+	}
+} else {
+	# no album with that name
+	# return 404 not found
+	not_found();
+}
 
-
-
-#$imagelol->disconnect();
-
+# done
+$imagelol->disconnect();
